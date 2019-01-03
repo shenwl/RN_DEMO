b8f043cfbda3c4bad88660717c70e995937e317e
shenwl <shenweilun96@gmail.com>
2019-01-03 20:39:40 +0800

    Android中的物理返回键处理方式

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,35 @@
 import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
+import {NavigationActions} from 'react-navigation';
 import NavigatorUtils from '../navigator/NavigatorUtils';
-
 import DynamicTabNavigator from '../navigator/DynamicTabNavigator';
+import {connect} from 'react-redux';
 
-export default class HomePage extends Component {
+class HomePage extends Component {
+  /**
+   * 处理Android中的物理返回键
+   * @returns {boolean}
+   */
+  onBackPress = () => {
+    const {dispatch, nav} = this.props;
+    if (nav.routes[0].index === 0) {
+      return false;
+    }
+    dispatch(NavigationActions.back());
+    return true;
+  };
 
   render() {
     NavigatorUtils.navigation = this.props.navigation;
-    return <DynamicTabNavigator></DynamicTabNavigator>;
+    return <DynamicTabNavigator />;
   }
 }
 
-const styles = StyleSheet.create({
+const mapStateToProps = state => {
+  return {
+    nav: state.nav,
+  };
+};
 
-});
+
+export default connect(mapStateToProps)(HomePage);
+
