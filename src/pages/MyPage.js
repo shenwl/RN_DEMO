51d8f2af7aa4630f3a4ba835b1548782ca423cef
shenwl <shenweilun96@gmail.com>
2019-01-03 20:08:47 +0800

    My页改变颜色

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
+import {connect} from 'react-redux';
+import actions from '../actions';
 
-type Props = {};
-export default class MyPage extends Component<Props> {
-
+class MyPage extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -11,18 +11,27 @@ export default class MyPage extends Component<Props> {
         <Button
           title="改变主题颜色"
           onPress={() => {
-            this.props.navigation.setParams({
-              theme: {
-                tintColor: 'red',
-                updateTime: new Date().getTime()
-              }
-            })
+            this.props.onThemeChange('red');
           }}
         ></Button>
       </View>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    theme: state.theme.theme,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onThemeChange: theme => dispatch(actions.onThemeChange(theme)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyPage);
 
 const styles = StyleSheet.create({
   container: {
