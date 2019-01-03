d059ce670c5b45bab7c7f699c825d0551b68999d
shenwl <shenweilun96@gmail.com>
2019-01-03 20:31:16 +0800

    如何自定义redux中间件

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,21 @@ import thunk from 'redux-thunk';
 import reducers from '../reducers';
 import {middleware} from "../navigator/AppNavigator";
 
+// 自定义中间件
+const logger = store => next => action => {
+  if (typeof action === 'function') {
+    console.log('dispatching a function');
+  } else {
+    console.log('dispatching ', action);
+  }
+  const result = next(action);
+  console.log('next state', store.getState());
+};
 
 const middleWares = [
   middleware,
-  thunk
+  thunk,
+  logger,
 ];
 
 export default createStore(reducers, applyMiddleware(...middleWares));
