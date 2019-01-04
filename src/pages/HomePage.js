8811d5c2585f8689e8f8c74ffd37ad0dc4d45b26
shenwl <shenweilun96@gmail.com>
2019-01-04 21:31:26 +0800

    Android物理返回键监听

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,27 @@
 import React, {Component} from 'react';
+import {BackHandler} from 'react-native';
 import {NavigationActions} from 'react-navigation';
 import NavigatorUtils from '../navigator/NavigatorUtils';
 import DynamicTabNavigator from '../navigator/DynamicTabNavigator';
 import {connect} from 'react-redux';
 
 class HomePage extends Component {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
   /**
    * 处理Android中的物理返回键
    * @returns {boolean}
    */
   onBackPress = () => {
     const {dispatch, nav} = this.props;
-    if (nav.routes[0].index === 0) {
+    // 如果RootNavigator中的MainNavigator的index为0，则不处理返回事件
+    if (nav.routes[1].index === 0) {
       return false;
     }
     dispatch(NavigationActions.back());
@@ -20,7 +30,7 @@ class HomePage extends Component {
 
   render() {
     NavigatorUtils.navigation = this.props.navigation;
-    return <DynamicTabNavigator />;
+    return <DynamicTabNavigator/>;
   }
 }
 
